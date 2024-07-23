fe1c9c9155860221d0071c5c5ecb2f2d8f972a83
THWACK <dynastycloudxiv@gmail.com>
2024-07-23 01:44:06 -0400

    first commit; switcharoo from touhou to snowbreak * removed all touhou project datasets * removed all touhou project assets * added snowbreak character image assets * added icon, banner image, and left and right block images * uploaded files to catbox instead of imgur; load directly from github instead? * added character data for all main operatives; need to add npcs next * slight touchups to index.html * increased image display count to 16 so all operative images can be seen at the same time

---
+++ src/js/data/2024-07-22.js
@@ -0,0 +1,125 @@
+dataSetVersion = "2024-07-22"
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    {
+        name: "Disable NPC Characters",
+        key: "npc",
+        tooltip: "Doesn't do anything, haven't added NPCs yet."
+    }
+]
+
+dataSet[dataSetVersion].characterData = [
+    {
+        name: "Acacia",
+        img: "v9ktyt.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Cherno Kegaard",
+        img: "10wbqw.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Eatchel Gustav",
+        img: "44g7c7.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Enya Murphy",
+        img: "jrl8b3.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Fenny Golden",
+        img: "w5z7xa.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Fritia Ignis",
+        img: "edhi91.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Ji Chenxing",
+        img: "ru6p2f.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Katya Klein",
+        img: "2wopu4.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Lyfe Bestla",
+        img: "42kr69.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Marian Andreotti",
+        img: "y9855m.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Mauxir",
+        img: "q39lky.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Naruse Haru",
+        img: "d9ohai.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Nita",
+        img: "qhnyc8.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Siris",
+        img: "0y1mg1.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Tess Kotkin",
+        img: "i2n0f5.png",
+        opts: {
+            npc: false
+        }
+    },
+    {
+        name: "Yao",
+        img: "y7fb8f.png",
+        opts: {
+            npc: false
+        }
+    },
+]
